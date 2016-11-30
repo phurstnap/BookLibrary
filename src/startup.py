73abc7b619835a69eeff2e09d170c00797bee64d
phurstnap <firerider@live.co.uk>
2016-11-30 12:40:14 +0000

    missed a ,

--- src/startup.py
+++ src/startup.py
@@ -18,7 +18,7 @@ def close_db_connection(exception):
 def init_db():
 	with app.app_contect():
 		db = get_db();
-		with app.open_resource('schema.sql' mode='r') as f:
+		with app.open_resource('schema.sql', mode='r') as f:
 			db.cursor().executescript(f.read())
 		db.commit()
 
