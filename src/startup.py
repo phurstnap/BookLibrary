61e242706c6347ab0eb9a4f1fca2e8d1c6773ff5
phurstnap <firerider@live.co.uk>
2016-11-30 15:54:36 +0000

    added session

--- src/startup.py
+++ src/startup.py
@@ -4,21 +4,29 @@ app = Flask(__name__)
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-	if request.method=='POST':
-		username = request.form['username']
-		password = request.form['password']
-		
-		con = sql.connect("users.db")
-		cur = con.cursor()
-		cur.execute('SELECT * from users WHERE username="%s" AND password="%s"' % (username, password))
-		user = cur.fetchone()
-		
-		con.commit()
-		con.close()
-		
-		return render_template('user.html', id=None, user = user, password=None)
-	else:
-		return render_template('login.html')
+	try:
+		if(session['name'):
+			return render_template('user.html', user = name)
+		else:
+			if request.method=='POST':
+				username = request.form['username']
+				password = request.form['password']
+				
+				con = sql.connect("users.db")
+				cur = con.cursor()
+				cur.execute('SELECT * from users WHERE username="%s" AND password="%s"' % (username, password))
+				user = cur.fetchone()
+				
+				con.commit()
+				con.close()
+				if c.fetchone() is not None:
+					session['name'] = user
+					return render_template('user.html', id=None, user = user, password=None)
+				else
+				return render_template('login.html')
+			else:
+				return render_template('login.html')
+			
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
@@ -35,7 +43,7 @@ def register():
 		return render_template('register.html')
 		
 @app.route('/user')
-def user(name = None, password = None):
+def user(name = None):
 	return render_template('user.html')
 
 @app.route('/')
