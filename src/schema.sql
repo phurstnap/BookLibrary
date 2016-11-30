aa3d18bfef289be107611a8f23f5bca31c41feaf
phurstnap <firerider@live.co.uk>
2016-11-30 14:16:06 +0000

    fixing mistake

--- src/schema.sql
+++ src/schema.sql
@@ -1,7 +1,6 @@
-DROP TABLE if EXISTS users;
-
-CREATE TABLE users (
+drop table if exists users;
+	create table users (
 	id integer primary key autoincrement,
 	username text not null,
-	password text not null,
+	password text not null
 );
