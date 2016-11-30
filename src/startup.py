7da8a6de5ffd87c96cd88d4bc15d7c4269959633
phurstnap <firerider@live.co.uk>
2016-11-30 19:45:42 +0000

    added logging and image transfer

--- src/startup.py
+++ src/startup.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, request
+from logging.handlers import RotatingFileHandler
 import sqlite3 as sql
+import ConfigParser
+import logging
 app = Flask(__name__)
 
 @app.route('/login/', methods=['GET', 'POST'])
@@ -58,6 +61,7 @@ def user(username = None):
 						Author: {{author}}</br>
 						Page: {{page}}</br>
 						Line: {{line}}</br>
+						<img src="{{ url_for('static', filename='img/{{title}}.png') }}">
 					</form>
 				</div>
 		'''
@@ -82,6 +86,9 @@ def bookmark(username = None):
 			page = request.form['page']
 			line = request.form['line']
 			
+			f = request.files['datafile']
+			f.save('static/uploads/' + title + '.png')
+			
 			conb = sql.connect("books.db")
 			curb = conb.cursor()
 			curb.execute("INSERT INTO books (username, title, author, page, line) VALUES (?,?,?,?,?)", (username, title, author, page, line))
@@ -90,6 +97,7 @@ def bookmark(username = None):
 		else:
 			return render_template('bookmark.html')
 		con.close()
+		
 		
 	else:
 		return render_template('bookmark.html')
@@ -101,7 +109,48 @@ def redirects():
 @app.errorhandler(404)
 def handler404(e):
 	return render_template('404.html')
+	
+@app.route('/config/')
+def config():
+	str = []
+	str.append('Debug: ' + app.config['DEBUG'])
+	str.append('Port: ' + app.config['port'])
+	str.append('Url: ' + app.config['url'])
+	str.append('IP_address: ' + app.config['ip_address'])
+	return '\t'.join(str)
 
+def init(app):
+	config = ConfigParser.ConfigParser()
+	try:
+		config_location = "etc/defaults.cfg"
+		config.read(config_location)
+		
+		app.config['DEBUG'] = config.get("config", "debug")
+		app.config['ip_address'] = config.get("config", "ip_address")
+		app.config['port'] = config.get("config", "port")
+		app.config['url'] = config.get("config", "url")
+		
+		app.config['log_file'] = config.get("logging", "name")
+		app.config['log_location'] = config.get("logging", "location")
+		app.config['log_level'] = config.get("logging", "level")
+
+	except:
+		print "Could not read from configs at: " + config_location
+
+def logs(app)
+	log_pathname = app.config['log_location'] + app.config ['log_file']
+	file_handler = RotatingFileHandler(log_pathname, maxByte = 1024 * 1024 * 10, backupCount = 1024)
+	file_handler.setlevel(app.config['log_level'])
+	formatter = logging.Formatter("%(levelname)s | %(asctime)s | %(module)s | %(funcName)s | %(message)s")
+	file_handler.setFormatter(formatter)
+	app.logger.setLevel(app.config['log_level'])
+	app.logger.addHandler(file_handler)
 
 if __name__ == ("__main__"):
-	app.run(host = '0.0.0.0', debug=True)
+	init(app)
+	logs(app)
+	app.run
+	(
+		host = app.config['ip_address'],
+		port = int(app.config['port'])
+	)
