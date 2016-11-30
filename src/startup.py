b9e845438b0af8d1a4030a4ffa22c431e8ed1dfb
phurstnap <firerider@live.co.uk>
2016-11-30 17:44:33 +0000

    format update

--- src/startup.py
+++ src/startup.py
@@ -11,7 +11,7 @@ def login():
 		con = sql.connect("users.db")
 		cur = con.cursor()
 		cur.execute('SELECT username from users WHERE username="%s" AND password="%s"' % (username, password))
-		user.encode('utf-8') = cur.fetchone()
+		user = cur.fetchone()
 		
 		con.commit()
 		
