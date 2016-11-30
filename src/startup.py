ffa6c99172ee9645ce3e8318dd13eb67c7234094
phurstnap <firerider@live.co.uk>
2016-11-30 19:57:26 +0000

    L

--- src/startup.py
+++ src/startup.py
@@ -140,7 +140,7 @@ def init(app):
 def logs(app):
 	log_pathname = app.config['log_location'] + app.config ['log_file']
 	file_handler = RotatingFileHandler(log_pathname, maxBytes=1024 * 1024 * 10, backupCount= 1024)
-	file_handler.setlevel(app.config['log_level'])
+	file_handler.setLevel(app.config['log_level'])
 	formatter = logging.Formatter("%(levelname)s | %(asctime)s | %(module)s | %(funcName)s | %(message)s")
 	file_handler.setFormatter(formatter)
 	app.logger.setLevel(app.config['log_level'])
